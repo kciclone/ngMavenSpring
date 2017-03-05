77e44b0a934f89372ec4447caf449ddd179ac8f2
karthik <karthikgopalapuram@gmail.com>
2017-03-05 12:53:59 -0500

    Initial Changes

---
+++ Angular2Boot/backend/src/main/java/org/sample/karthik/Angular2BootApplication.java
@@ -0,0 +1,12 @@
+package org.sample.karthik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular2BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular2BootApplication.class, args);
+	}
+}
